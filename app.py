0f1d3f4f5ba3047572c74ee2adc4f114d617a2f9
Amit Verma <mt4descentis@gmail.com>
2022-07-07 16:46:18 +0530

    updated the app

--- app.py
+++ app.py
@@ -222,7 +222,7 @@ if Run_Button and st.session_state.boolean == False and len(selected_questions) 
                 )
                 predictions.append(prediction)
             for each in predictions:
-                st.write(each['answers'])
+                st.write(each['answers'][0])
         else:
             st.write("Stopping the function")
             predictions = ""
